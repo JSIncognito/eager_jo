b7de6dd445a0b8eb3f6cc2dfae131f98627ed1f7
Junsoo <junsoo3218@gmail.com>
2017-12-11 17:49:40 +0900

    프로젝트 최초 소스 등록

---
+++ fooder/src/com/ej/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.ej.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/main.ej")
+	public String main() {
+		return "main";
+	}
+}
