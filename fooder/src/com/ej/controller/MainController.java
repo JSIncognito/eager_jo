cf638a376302a9a359af01f13de001dfd0254045
Junsoo <junsoo3218@gmail.com>
2017-12-14 14:10:45 +0900

    fooder 수정사항 1. 부트스트랩 fooder 적용. 	view > store 	1) order1.jsp 	2) order2.jsp 	3) order3.jsp 	4) store_detail.jsp 	5) store_list.jsp
    	view 
    	1) main.jsp
    	2) center.jsp
    	
    2. 각각의 페이지 자바스크립트 등등 사항 main.jsp 적용.

--- fooder/src/com/ej/controller/MainController.java
+++ fooder/src/com/ej/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.ej.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +11,29 @@ public class MainController {
 	public String main() {
 		return "main";
 	}
-	@RequestMapping("/store_ist.ej")
-	public String store_list() {
-		return "store_list";
-	}	
+	@RequestMapping("/store_list.ej")
+	public String store_list(Model model) {
+		model.addAttribute("center", "store/store_list");
+		return "main";
+	}
+	@RequestMapping("/order1.ej")
+	public String order1(Model model) {
+		model.addAttribute("center", "store/order1");		
+		return "main";
+	}
+	@RequestMapping("order2.ej")
+	public String order2(Model model) {
+		model.addAttribute("center","store/order2");
+		return "main";
+	}
+	@RequestMapping("order3.ej")
+	public String order3(Model model) {
+		model.addAttribute("center", "store/order3");
+		return "main";
+	}
+	@RequestMapping("store_detail")
+	public String store_detail(Model model) {
+		model.addAttribute("center", "store/store_detail");
+		return "main";
+	}
 }
