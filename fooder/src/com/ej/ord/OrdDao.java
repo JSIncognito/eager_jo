b770f05c9017dfa8346bf6f36ace45d52ef65d39
hb <blakiron.lyu@gmail.com>
2017-12-18 16:28:01 +0900

    1. Ord Biz/Dao/Mapper 생성

---
+++ fooder/src/com/ej/ord/OrdDao.java
@@ -0,0 +1,42 @@
+package com.ej.ord;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ej.frame.Dao;
+import com.ej.mapper.OrdMapper;
+import com.ej.vo.Ord;
+
+@Repository("ordDao")
+public class OrdDao implements Dao<Ord, Double> {
+
+	@Autowired
+	OrdMapper mapper;
+	
+	@Override
+	public void insert(Ord t) {
+		mapper.insert(t);
+	}
+
+	@Override
+	public void delete(Double s) {
+		mapper.delete(s);
+	}
+
+	@Override
+	public void update(Ord t) {
+		mapper.update(t);
+	}
+
+	@Override
+	public Ord select(Double s) {
+		return mapper.select(s);
+	}
+
+	@Override
+	public List<Ord> select() {
+		return mapper.selectall();
+	}
+}
