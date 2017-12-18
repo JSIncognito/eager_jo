72e37c9e43e2da0e4cb796846580f15a169a52ee
hb <blakiron.lyu@gmail.com>
2017-12-18 11:26:12 +0900

    1. Users / Ord Biz&dao&mapper 생성

--- fooder/src/com/ej/users/UsersBiz.java
+++ fooder/src/com/ej/users/UsersBiz.java
@@ -2,33 +2,48 @@ package com.ej.users;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.ej.frame.Biz;
+import com.ej.frame.Dao;
 import com.ej.vo.Users;
 
+@Service("usersBiz")
 public class UsersBiz implements Biz<Users, String> {
 
+	@Resource(name="usersDao")
+	Dao<Users, String> dao;
+	
+	@Transactional
 	@Override
 	public void register(Users t) {
-		
+		dao.insert(t);
 	}
 
+	@Transactional
 	@Override
 	public void remove(String s) {
-		
+		dao.delete(s);
 	}
 
+	@Transactional
 	@Override
 	public void modify(Users t) {
-		
+		dao.update(t);
 	}
 
 	@Override
 	public Users get(String s) {
-		return null;
+		return dao.select(s);
 	}
 
 	@Override
 	public List<Users> get() {
-		return null;
+		return dao.select();
 	}
+
+
 }
