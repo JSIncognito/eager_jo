720fc4195ad9d1d6a2242e9fc2d4fad4f62ff729
hb <gksquf0737@naver.com>
2017-12-20 09:14:52 +0900

    한별 1. OrderController 작성

---
+++ fooder/src/com/ej/controller/OrderController.java
@@ -0,0 +1,99 @@
+package com.ej.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.jdbc.support.incrementer.DataFieldMaxValueIncrementer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ej.frame.Biz;
+import com.ej.vo.Coupon;
+import com.ej.vo.Food;
+import com.ej.vo.Ofd;
+import com.ej.vo.Ord;
+import com.ej.vo.Users;
+
+@Controller
+public class OrderController {
+	@Resource(name="usersBiz")
+	Biz<Users, String> userbiz;
+	
+	 private DataFieldMaxValueIncrementer incrementer;
+	 public void setIncrementer(DataFieldMaxValueIncrementer incrementer) {
+		 this.incrementer = incrementer;
+	 }
+	 
+	
+	 // 가게 이름, 주문 메뉴 목록, 사용 쿠폰 받아옴
+	@RequestMapping("/order1.ej")
+	public String order1(HttpServletRequest request, Ord ord) {
+		// 세션에 있던 기존 주문 정보, ofd 목록 삭제
+		HttpSession session = request.getSession();
+		session.removeAttribute("orderInfo");	// TODO : 삭제 잘 되는지 확인하기
+		session.removeAttribute("ofdList");
+		
+		// 주문정보 만들기
+//		Ord ord = new Ord();
+		
+		// 가게 이름 받아와서 설정
+//		ord.setSt_nm((String) request.getAttribute("st_nm"));		// 협의 필요
+		int maxID = new Integer(incrementer.nextIntValue());
+		ord.setO_key(maxID);
+		
+		// 주문 메뉴 목록 가져오기
+		List<Food> foods = (List<Food>) request.getAttribute("foods");	//협의 필요
+		
+		// ofd 리스트 만들어주기 & 메뉴 총 금액 구하기
+		List<Ofd> ofds = new ArrayList<>();
+		double total = .0;
+		for(Food f : foods) {
+			Ofd ofd = new Ofd(maxID, f.getF_key(), f.getF_name(), f.getF_price(), f.getF_img(), f.getSt_key());
+			ofds.add(ofd);
+			total += f.getF_price();
+		}
+//		ord.setO_all(total);	// o_all(총 금액) 입력
+		
+		// 쿠폰 받아오기
+		Coupon gCpn = (Coupon) request.getAttribute("gCoupon");	// 협의 필요, 증정 쿠폰
+		Coupon dCpn = (Coupon) request.getAttribute("dCoupon");	// 협의 필요, 할인 쿠폰
+//		if(dCpn != null) {	// 할인 금액 설정
+//			ord.setO_dc(dCpn.getC_dc());
+//		} else {
+//			ord.setO_dc(.0);
+//		}
+//		ord.setO_total(ord.getO_all()-ord.getO_dc()); 	// 최종 결제 금액 설정
+		
+		// 주문 메뉴 목록 넘기기 
+		request.setAttribute("fList", foods);
+		
+		// 주문 정보, ofd 리스트 세션에 넣기
+		session.setAttribute("orderInfo", ord);
+		session.setAttribute("ofdList", ofds);
+		
+		request.setAttribute("center", "store/order1");
+		return "main";
+	}
+	
+	// 결제수단 받아옴
+	@RequestMapping("/order2.ej")
+	public String order2(HttpServletRequest request, Integer o_way) {
+		HttpSession session = request.getSession();
+		Ord ord = (Ord) session.getAttribute("orderInfo");
+		ord.setO_way(o_way);
+		session.setAttribute("orderInfo", ord); 	// TODO : 지우지 않고 바로 넣을 수 있는지 확인하기
+		
+		request.setAttribute("center", "store/order2");
+		return "main";
+	}
+	
+	@RequestMapping("/order3.ej")
+	public String order3(HttpServletRequest request) {
+		request.setAttribute("center", "store/order3");
+		return "main";
+	}
+}
